0d0137b139d6725799bd15fae4168db33dcdaa33
Eden Benbow <37776140+edenbenbow@users.noreply.github.com>
2019-12-04 08:52:07 -0800

    Replaced existing content with Pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
-alert('Hello world');
+let repository = [
+  {
+    name: 'Bellossom',
+    height: .4,
+    types: ['grass']
+  },
+  {
+    name: 'Parasect',
+    height: .99,
+    types: ['bug', 'grass']
+  },
+  {
+    name: 'Pichu',
+    height: .3,
+    types: ['electric']
+  },
+  {
+    name: 'Sealeo',
+    height: 1.09,
+    types: ['ice', 'water']
+  }
+];
 
-let favoriteFood = 'moussaka';
-
-document.write(favoriteFood);
+   
