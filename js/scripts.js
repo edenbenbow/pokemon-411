8590021ec8f00b7309d48a53cfcd19e1527bb4b1
Eden Benbow <37776140+edenbenbow@users.noreply.github.com>
2019-12-08 17:35:06 -0800

    IIFE format; added getAll + add functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,49 @@
-const repository = [
-  {
-    name: 'Bellossom',
-    height: .41,
-    types: ['grass']
-  },
-  {
-    name: 'Parasect',
-    height: .99,
-    types: ['bug', 'grass']
-  },
-  {
-    name: 'Pichu',
-    height: .30,
-    types: ['electric']
-  },
-  {
-    name: 'Sealeo',
-    height: 1.09,
-    types: ['ice', 'water']
-  }
-];
+let pokemonRepository = (function(){
+    const repository = [
+    {
+      name: 'Bellossom',
+      height: .41,
+      types: ['grass']
+    },
+    {
+      name: 'Parasect',
+      height: .99,
+      types: ['bug', 'grass']
+    },
+    {
+      name: 'Pichu',
+      height: .30,
+      types: ['electric']
+    },
+    {
+      name: 'Sealeo',
+      height: 1.09,
+      types: ['ice', 'water']
+    }
+  ];
 
-
-repository.forEach(function(item) {
-  Object.keys(item).forEach(function(key) {
-    console.log(key + ": " + item[key]);
+  repository.forEach(function(item) {
+    Object.keys(item).forEach(function(key) {
+      console.log(key + ": " + item[key]);
+    });
   });
-});
+
+  function getAll() {
+    return repository;
+  }
+
+  function add(pokemon) {
+    if (typeof pokemon === "object"){
+      repository.push(pokemon);
+    }
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
 
 
 
