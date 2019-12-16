e5824a7271dae746fc60c61379ce63c3a3302cd1
Eden Benbow <37776140+edenbenbow@users.noreply.github.com>
2019-12-15 20:25:42 -0800

    Adding modals

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const pokemonRepository = (function(){
     const repository = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let $modalContainer = document.querySelector('#modal-container');
+    let $pokemonList = document.querySelector('ul');
 
   //Pulls all Pokemon data
 
@@ -12,14 +14,13 @@ const pokemonRepository = (function(){
 
   function showDetails(pokemon) {
       pokemonRepository.loadDetails(pokemon).then(function(){
-          console.log(pokemon);
-      })
+          showModal(pokemon);
+      });
   }
 
   //Adds character item to list (w/button and event listener)
 
   function addListItem(pokemon) {
-      let $pokemonList = document.querySelector('ul');
       let $listItem = document.createElement('li');
       let $button = document.createElement('button');
       $button.innerText = pokemon.name;
@@ -63,11 +64,71 @@ const pokemonRepository = (function(){
             // Now we add the details to the item
             item.imageUrl = details.sprites.front_default;
             item.height = details.height;
-            item.types = Object.keys(details.types);
+            item.types = "";
+            Object.values(details.types).forEach(function (item2){
+                item.types += (item.types.length !== 0?", ":"") + item2.type.name;
+            });
         }).catch(function (e) {
             console.error(e);
         });
     }
+
+    function showModal(pokemon) {
+
+      //Clear all existing modal content
+      $modalContainer.innerHTML = '';
+
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+
+        let modalCloseButton = document.createElement('button');
+        modalCloseButton.classList.add('modal-close');
+        modalCloseButton.innerText = 'Close';
+        modalCloseButton.addEventListener('click', hideModal);
+
+        let modalDetails = document.createElement('p');
+        modalDetails.innerText = 'They are ' + pokemon.height/10 + ' meters tall!';
+        modalDetails.classList.add('modalSubDetails');
+
+        let modalName = document.createElement('h2');
+        modalName.innerText = pokemon.name;
+        modalName.classList.add('modal-title');
+
+        let modalImg = document.createElement('Img');
+        modalImg.src = pokemon.imageUrl;
+        modalImg.classList.add('modal-img');
+
+        let modalSubDetails = document.createElement('p');
+        modalSubDetails.innerText = 'Types: ' + pokemon.types;
+        modalSubDetails.classList.add('modalSubDetails');
+
+        modal.appendChild(modalCloseButton);
+        modal.appendChild(modalName);
+        modal.appendChild(modalImg);
+        modal.appendChild(modalDetails);
+        modal.appendChild(modalSubDetails);
+        $modalContainer.appendChild(modal);
+
+        $modalContainer.classList.add('is-visible');
+    }
+
+    function hideModal() {
+      $modalContainer.classList.remove('is-visible');
+    }
+
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+            hideModal();
+        }
+        });
+
+    $modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === $modalContainer) {
+            hideModal();
+        }
+    });
+
 
     return {
       add: add,
@@ -75,13 +136,14 @@ const pokemonRepository = (function(){
       getAll: getAll,
       showDetails: showDetails,
       loadList: loadList,
-      loadDetails: loadDetails
+      loadDetails: loadDetails,
+      showModal: showModal,
+      hideModal: hideModal
     };
 })();
 
-
 pokemonRepository.loadList().then(function() {
-    pokemonRepository.getAll().forEach(function (pokemon) {
-        pokemonRepository.addListItem(pokemon);
+    pokemonRepository.getAll().forEach(function (pokeList) {
+        pokemonRepository.addListItem(pokeList);
     });
 });
